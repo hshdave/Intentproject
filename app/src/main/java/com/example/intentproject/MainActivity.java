103e490bb312a1d43a5961e255d8c89f83088d4c
hdave <hshdave2011>
2019-06-11 10:31:41 -0400

    AlertDialog Box and Intent with Extras

--- app/src/main/java/com/example/intentproject/MainActivity.java
+++ app/src/main/java/com/example/intentproject/MainActivity.java
@@ -1,10 +1,14 @@
 package com.example.intentproject;
 
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -23,11 +27,49 @@ public class MainActivity extends AppCompatActivity {
         btn_submit = findViewById(R.id.btn_submit);
 
 
+        final AlertDialog.Builder alertDialog = new AlertDialog.Builder(this);
+
         btn_submit.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
 
+                alertDialog.setMessage(R.string.alert);
+                alertDialog.setTitle("Demo!");
+
+                alertDialog.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+
+
+
+                        String uname,pass;
+
+                        uname = edt_uname.getText().toString();
+                        pass = edt_pass.getText().toString();
+
+                        Intent i = new Intent(MainActivity.this,dashboard.class);
+                        i.putExtra("un",uname);
+                        i.putExtra("ps",pass);
+                        startActivity(i);
+
+
+                        Toast.makeText(getApplicationContext(),uname+" Thanks!",Toast.LENGTH_LONG).show();
+
+                    }
+                });
+
+                alertDialog.setNegativeButton("No", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+
+                        Toast.makeText(getApplicationContext(),"Ok!",Toast.LENGTH_LONG).show();
+
+                    }
+                });
+
+
+                alertDialog.show();
 
             }
         });
--- app/src/main/java/com/example/intentproject/dashboard.java
+++ app/src/main/java/com/example/intentproject/dashboard.java
@@ -1,13 +1,28 @@
 package com.example.intentproject;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class dashboard extends AppCompatActivity {
 
+    String name,pass;
+    TextView txt_nm,txt_ps;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
+
+        Intent i = getIntent();
+         name = i.getStringExtra("un");
+         pass = i.getStringExtra("ps");
+
+
+         txt_nm = findViewById(R.id.txt_name);
+         txt_ps = findViewById(R.id.txt_pass);
+
+         txt_nm.setText(name);
+         txt_ps.setText(pass);
     }
 }
